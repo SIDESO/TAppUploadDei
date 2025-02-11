050d1b2e6cb2a7f7f70702e329c28a41e96c1dc7
villeracodecwork <villeraluiswork@gmail.com>
2025-02-11 12:51:39 -0500

    proyecto inicial

---
+++ FormStart.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace TappUploadDei
+{
+    public partial class FormSelectApp : Form
+    {
+        public FormSelectApp()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonVitronicApp_Click(object sender, EventArgs e)
+        {
+
+            //creamos una instancia de la clase FormVitronicApp
+            FormMainVitronic formVitronicApp = new FormMainVitronic();
+            //mostramos el formulario
+
+            formVitronicApp.Show();
+            //cerramos el formulario actual  sin finalizar la aplicacion
+            this.Dispose(false);
+
+
+        }
+    }
+}
